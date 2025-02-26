44e21a58b25f2c82b0b2a1f124d59d6d755b989b
srijansa <159839387+srijansa@users.noreply.github.com>
2025-02-26 02:17:54 -0500

    Routes Added

--- src/App.js
+++ src/App.js
@@ -1,38 +1,7 @@
-import Directory from "./components/directory/directory.component";
+import Home from "./routes/home/home.component";
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'Hats',
-      imageUrl: "/images/hats.png"
-    },
-    {
-      id: 2,
-      title: 'Jackets',
-      imageUrl: "/images/jackets.jpg"
-    },
-    {
-      id: 3,
-      title: 'Sneakers',
-      imageUrl: "/images/sneakers.png"
-    },
-    {
-      id: 4,
-      title: 'Mens',
-      imageUrl: "/images/mens.jpg"
-    },
-    {
-      id: 5,
-      title: 'Women',
-      imageUrl: "/images/women.jpg"
-    },
-  ]
-  return (
-    <div className="App">
-      <Directory categories={categories}/>
-    </div>
-  );
+  return <Home />
 }
 
 export default App;
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,38 @@
+import Directory from "../../components/directory/directory.component";
+
+const Home = () =>{
+    const categories = [
+        {
+          id: 1,
+          title: 'Hats',
+          imageUrl: "/images/hats.png"
+        },
+        {
+          id: 2,
+          title: 'Jackets',
+          imageUrl: "/images/jackets.jpg"
+        },
+        {
+          id: 3,
+          title: 'Sneakers',
+          imageUrl: "/images/sneakers.png"
+        },
+        {
+          id: 4,
+          title: 'Mens',
+          imageUrl: "/images/mens.jpg"
+        },
+        {
+          id: 5,
+          title: 'Women',
+          imageUrl: "/images/women.jpg"
+        },
+    ]
+
+    return (
+    <div className="App">
+        <Directory categories={categories}/>
+    </div>
+    );
+}
+export default Home;
