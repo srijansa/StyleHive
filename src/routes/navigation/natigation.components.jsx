80b8fadfdcec5018a12c2cd997f1639e0305d15c
srijansa <159839387+srijansa@users.noreply.github.com>
2025-02-26 04:16:42 -0500

    Navigation and Logo Added

--- src/routes/navigation/natigation.components.jsx
+++ src/routes/navigation/natigation.components.jsx
@@ -1,13 +1,23 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
+import { Fragment } from 'react';
+import { ReactComponent as StyleHiveLogo} from '../../assets/logo-style-hive.svg';
+import './navigation.styles.scss';
 
 const Navigation = () =>{
     return(
-      <>
-        <div>
-          <h1>Navigation Bar</h1>
+      <Fragment>
+        <div className='navigation'>
+            <Link className='logo-container' to='/'>
+                <StyleHiveLogo className='logo'/>
+            </Link>
+            <div className='nav-links-container'>
+                <Link className='nav-link' to='/shop'>
+                    SHOP
+                </Link>
+            </div>
         </div>
         <Outlet/>
-      </>
+      </Fragment>
     );
 }
 export default Navigation;
