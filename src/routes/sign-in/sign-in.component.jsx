716c5f9de113a9e37de690645f985f1eb13ce506
srijansa <159839387+srijansa@users.noreply.github.com>
2025-02-28 01:27:19 -0500

    refactored sig-in page

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,38 +1,14 @@
-import { useEffect } from "react";
-import { auth, signInWithGooglePopup, signInWithGoogleRedirect, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
-import { getRedirectResult } from "firebase/auth";
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 import './sign-in.styles.scss';
 const SignIn = () => {
-    // useEffect(async () =>{
-    //     const response = await getRedirectResult(auth);
-
-    //     if (response){
-    //         // When we use google redirect the component redirects i.e. unmounts and then it comes again 
-    //         const userDocRef = await createUserDocumentFromAuth(response.user);
-    //     }
-
-    // }, []);
-
-    // const logGoogleUser = async () => {
-    //   const { user } = await signInWithGooglePopup();
-    //   const userDocRef = await createUserDocumentFromAuth(user);
-    // };
-
-// const logGoogleRedirectUser = async() =>{
-//     const { user } = await signInWithGoogleRedirect();
-//     console.log({user});
-// };
   return (
     <div>
       <h1>Sign in Page</h1>
-      {/* <button onClick={logGoogleUser}>Sign in with Google Pop-up</button> */}
-      <div className='authentication-container'>
-        <SignInForm />
-        <SignUpForm />
-      </div>
-      {/* <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button> */}
+        <div className='authentication-container'>
+          <SignInForm />
+          <SignUpForm />
+        </div>
     </div>
   );
 };
