155d58209f505d169f99fbf9a2bcb4f852d06445
srijansa <159839387+srijansa@users.noreply.github.com>
2025-03-05 03:17:46 -0500

    Setting up data for database and without database

--- src/context/products.context.jsx
+++ src/context/products.context.jsx
@@ -1,23 +1,23 @@
  import { createContext, useState, useEffect } from "react";
  import PRODUCTS from '../shop-data.js';
 //  import { addCollectionAndDocuments }  from "../utils/firebase/firebase.utils";
-import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
+// import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
 
  export const ProductsContext = createContext({
-    products: [],
+    products: {},
  });
 
  export const ProductsProvider = ({children}) => {
-    const [products, setProducts] = useState(PRODUCTS);
-    const value = { products };
-
+   const [categoriesMap, setProducts] = useState(PRODUCTS);
+   const value = { categoriesMap };
+   console.log(categoriesMap);
 //   useEffect(() => {
 //      addCollectionAndDocuments('collections', PRODUCTS);
 //   }, [])
-   useEffect(async () => {
-      const categoryMap = await getCategoriesAndDocuments('categories');
-      console.log(categoryMap);
-   }, []);
+   // useEffect(async () => {
+   //    const categoryMap = await getCategoriesAndDocuments('categories');
+   //    console.log(categoryMap);
+   // }, []);
 
     return (
         <ProductsContext.Provider value = {value}>{children}</ProductsContext.Provider>
