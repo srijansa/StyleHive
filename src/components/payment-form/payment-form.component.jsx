c7f439c1aba6c8401c8c17ebd16c4a1abf902c2b
srijansa <159839387+srijansa@users.noreply.github.com>
2025-03-14 21:21:17 -0400

    Adding scss for Payment Form

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,12 +1,16 @@
-import { CardElement } from "@stripe/react-stripe-js";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import Button, {BUTTON_TYPE_CLASSES} from '../button/button.component';
+import { PayementFormContainer, FormContainer } from './payment-form.styles';
 
 const PaymentForm = () =>{
     return (
-        <div>
-            <CardElement />
-            <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
-        </div>
+        <PayementFormContainer>
+            <FormContainer>
+                <h2>Credit Card Payment</h2>
+                <CardElement />
+                <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
+            </FormContainer>
+        </PayementFormContainer>
     )
 }
 export default PaymentForm;
---
+++ src/components/payment-form/payment-form.styles.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const PayementFormContainer = styled.div `
+    height: 300px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-contentL center;
+`;
+
+export const FormContainer = styled.form`
+    height: 100px;
+    min-width: 500px;
+`;
